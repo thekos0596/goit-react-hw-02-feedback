9b4393d9bb981f2c53bd53175d9d9a22b9dc58a0
Haidai Kostiantyn <thekos0596@gmail.com>
2023-04-13 22:07:21 +0300

    Create Fedback

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin: 0 auto;
+  margin-top: 20px;
+  padding: 20px;
+  width: 600px;
+  /* box-shadow: var(--main-shadow); */
+  border-radius: 10px;
+  background-color: blue;
+`;
---
+++ src/components/FedbackOptions/FedbackOption.styled.jsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+export const ButtonList = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+`;
+
+export const ButtonItem = styled.li`
+  :hover:nth-of-type(1) button {
+    background-color: green;
+  }
+  :hover:nth-of-type(2) button {
+    background-color: orange;
+  }
+  :hover:nth-of-type(3) button {
+    background-color: red;
+  }
+`;
+
+export const Button = styled.button`
+  padding: 5px 25px;
+  font-size: 24px;
+  color: var(--accent);
+  background-color: transparent;
+  border-radius: 5px;
+  border: 1px solid grey);
+  cursor: pointer;
+  :hover {
+    color: white;
+    border: 1px solid transparent;
+  }
+`;
